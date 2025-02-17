72910baf37ea8ae45441005c8d165ed836b764b1
Giioya <gioda2405@gmail.com>
2025-02-17 15:41:53 -0500

    finalf

--- app/api/obtener-referencia/route.ts
+++ app/api/obtener-referencia/route.ts
@@ -1,9 +1,9 @@
-// /pages/api/obtener-referencia.ts
+// app/api/obtener-referencia/route.ts
 
-import { NextApiRequest, NextApiResponse } from 'next';
 import { supabase } from '../../lib/supabase'; // Ajusta la ruta según sea necesario
+import { NextResponse } from 'next/server';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function GET() {
     try {
         // Obtén el código de referencia de la última transacción registrada (ajusta según tu lógica)
         const { data, error } = await supabase
@@ -18,15 +18,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
 
         if (data) {
-            return res.status(200).json({ referencia: data.reference });
+            return NextResponse.json({ referencia: data.reference });
         }
 
-        return res.status(404).json({ message: 'Referencia no encontrada' });
+        return NextResponse.json({ message: 'Referencia no encontrada' }, { status: 404 });
     } catch (error) {
         console.error('Error al obtener referencia:', error);
-        return res.status(500).json({ message: 'Error en el servidor' });
+        return NextResponse.json({ message: 'Error en el servidor' }, { status: 500 });
     }
 }
+
 
 
 
