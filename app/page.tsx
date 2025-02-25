d600b2a709d7c1dab64033e3d1dea92473d876ee
Giioya <gioda2405@gmail.com>
2025-02-24 19:51:36 -0500

    precioo

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
         const tasaCambioCOP: number = 4060;
         const valorWLDenCOP: number = valorWLDUSD * tasaCambioCOP;
 
-        const valorConDescuento: number = valorWLDenCOP * 0.9;
+        const valorConDescuento: number = valorWLDenCOP * 0.92;
         const valorTotal: number = valorConDescuento * cantidadWLD;
 
         if (isNaN(valorTotal)) {
