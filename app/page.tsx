2e60dc91cc987958030f6b6589441c6c6044999e
Giioya <gioda2405@gmail.com>
2025-03-15 19:51:14 -0500

    saldo al inicio

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Image from 'next/image';
-import monedaEnviarImg from '@/public/images/wld-logo.png';
-import dineroRecibirImg from '@/public/images/colombia-flag.png';
+import Image from "next/image";
+import monedaEnviarImg from "@/public/images/wld-logo.png";
+import dineroRecibirImg from "@/public/images/colombia-flag.png";
 import { useWalletAuth } from "@/components/wallet/";
 import { getBalance } from "@/components/balance";
 
-// Función para redirigir según el método de pago
 const redirigirSegunMetodoPago = (
   setErrorMessage: React.Dispatch<React.SetStateAction<string | null>>,
-  datos: { cantidadWLD: number, metodoPago: string, dineroARecibir: string }
+  datos: { cantidadWLD: number; metodoPago: string; dineroARecibir: string }
 ) => {
   const { cantidadWLD, metodoPago, dineroARecibir } = datos;
 
   if (!metodoPago || isNaN(cantidadWLD) || cantidadWLD <= 0 || !dineroARecibir) {
-    setErrorMessage('Por favor completa los espacios.');
+    setErrorMessage("Por favor completa los espacios.");
     return;
   }
 
@@ -41,17 +40,16 @@ export default function Home() {
     if (walletAddress) {
       getBalance(walletAddress).then((saldo) => {
         console.log("Saldo disponible en WLD:", saldo);
-        setSaldoDisponible(saldo);
+        setSaldoDisponible(parseFloat((saldo * 0.996).toFixed(2))); // Restando 0.004%
       });
     }
   }, [walletAddress]);
 
-  // Obtener precios de WLD y USDT
   const actualizarPrecios = async () => {
     try {
       const [wldResponse, usdtResponse] = await Promise.all([
         fetch(`https://api.coingecko.com/api/v3/simple/price?ids=worldcoin-wld&vs_currencies=usd&_=${Date.now()}`),
-        fetch(`https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=cop&_=${Date.now()}`)
+        fetch(`https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=cop&_=${Date.now()}`),
       ]);
       const wldData = await wldResponse.json();
       const usdtData = await usdtResponse.json();
@@ -59,7 +57,7 @@ export default function Home() {
       setPrecioWLD(wldData["worldcoin-wld"]?.usd || null);
       setPrecioUSDT(usdtData.tether?.cop || null);
     } catch (error) {
-      console.error('Error al obtener los precios:', error);
+      console.error("Error al obtener los precios:", error);
       setPrecioWLD(null);
       setPrecioUSDT(null);
     }
@@ -67,19 +65,20 @@ export default function Home() {
 
   useEffect(() => {
     actualizarPrecios();
-    const interval = setInterval(actualizarPrecios, 300000); // 5 minutos
+    const interval = setInterval(actualizarPrecios, 300000);
     return () => clearInterval(interval);
   }, []);
 
-  // Calcular el valor en COP cuando cambie `cantidadWLD`
   useEffect(() => {
     if (precioWLD !== null && precioUSDT !== null && cantidadWLD > 0) {
-      const precioUSDTajustado = precioUSDT - 90; // Restar 90 COP al precio del USDT
+      const precioUSDTajustado = precioUSDT - 90;
       const valorWLDenCOP = precioWLD * precioUSDTajustado;
       const descuento = cantidadWLD < 1 ? 0.5 : 0.9;
       const valorTotal = valorWLDenCOP * descuento * cantidadWLD;
 
-      setDineroARecibir(valorTotal.toLocaleString('es-CO', { minimumFractionDigits: 0, maximumFractionDigits: 0 }));
+      setDineroARecibir(
+        valorTotal.toLocaleString("es-CO", { minimumFractionDigits: 0, maximumFractionDigits: 0 })
+      );
     } else {
       setDineroARecibir("");
     }
@@ -94,13 +93,24 @@ export default function Home() {
             ⚠ Si tienes problemas con tu transacción, dirígete al apartado de &quot;Ayuda&quot; o escríbenos a soporte.
           </p>
           <p className="font-semibold">
-            * Las transacciones que se realicen después de las 10pm se verán reflejadas a las 9am del día siguiente.
+            * Las transacciones que se realicen después de las 10:00 PM se verán reflejadas a las 9:00 AM del día siguiente.
           </p>
         </div>
       </div>
+      {/* Saldo disponible con formato mejorado */}
+      <div className="flex flex-col items-center mt-4">
+        <p className="text-gray-500 text-lg font-semibold">Saldo disponible para retiro:</p>
+        <p className="text-4xl font-bold">
+          {saldoDisponible.toLocaleString("es-CO", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          }).split(".")[0]} WLD
+          <span className="text-gray-400">
+          </span>
+        </p>
+      </div>
 
       <div className="container">
-        {/* Mensaje de bienvenida */}
         <div className="text-center text-xl font-bold my-4">
           {username ? `Bienvenido, ${username}` : `Bienvenido, ${walletAddress?.slice(0, 6)}...`}
         </div>
@@ -113,43 +123,48 @@ export default function Home() {
               type="number"
               step="0.1"
               id="moneda_a_enviar"
-              value={cantidadWLD || ''}
+              value={cantidadWLD || ""}
               onChange={(e) => setCantidadWLD(parseFloat(e.target.value) || 0)}
               placeholder="Cantidad en WLD"
             />
           </div>
-          {saldoDisponible > 0 && (
-            <p 
-              className="text-blue-600 text-sm cursor-pointer underline mt-1"
-              onClick={() => setCantidadWLD(parseFloat((saldoDisponible * 0.996).toFixed(2)))}
-            >
-              MAX ({(saldoDisponible * 0.996).toFixed(2)} WLD)
-            </p>
-          )}
+          <p
+            className="text-blue-600 text-sm cursor-pointer underline mt-1"
+            onClick={() => setCantidadWLD(saldoDisponible)}
+          >
+            MAX
+          </p>
         </div>
 
         <div className="input-group">
           <label htmlFor="dinero_a_recibir">Dinero a recibir</label>
           <div className="input-wrapper">
             <Image src={dineroRecibirImg.src} alt="Dinero a recibir" className="input-icon" width={24} height={24} />
-            <input type="text" id="dinero_a_recibir" value={dineroARecibir || ''} placeholder="Cantidad en COP" readOnly />
+            <input type="text" id="dinero_a_recibir" value={dineroARecibir || ""} placeholder="Cantidad en COP" readOnly />
           </div>
         </div>
 
         <div className="input-group">
           <label htmlFor="metodo-pago">Método de pago</label>
           <select id="metodo-pago" value={metodoPago} onChange={(e) => setMetodoPago(e.target.value)}>
-            <option value="" disabled>Selecciona un banco</option>
+            <option value="" disabled>
+              Selecciona un banco
+            </option>
             <option value="nequi">Nequi</option>
             <option value="daviplata">Daviplata</option>
             <option value="bancolombia">Bancolombia</option>
           </select>
         </div>
 
-        {errorMessage && <div style={{ color: 'red', marginTop: '10px', marginBottom: '20px' }}>{errorMessage}</div>}
+        {errorMessage && (
+          <div style={{ color: "red", marginTop: "10px", marginBottom: "20px" }}>{errorMessage}</div>
+        )}
 
         <div className="btn-continuar">
-          <button onClick={() => redirigirSegunMetodoPago(setErrorMessage, { cantidadWLD, metodoPago, dineroARecibir })} type="submit">
+          <button
+            onClick={() => redirigirSegunMetodoPago(setErrorMessage, { cantidadWLD, metodoPago, dineroARecibir })}
+            type="submit"
+          >
             Continuar
           </button>
         </div>
