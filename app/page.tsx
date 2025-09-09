f398c88c419df662553a0e84f812b1173d082f02
Giioya <giovanny_316@hotmail.com>
2025-09-09 07:31:56 -0500

    error pending

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,13 @@ export default function Home() {
       <div className="w-full max-w-2xl mb-4 flex justify-center">
         <div className="bg-yellow-50 border-l-4 border-yellow-500 text-red-600 text-sm p-2 rounded-md shadow-md text-center">
           <p className="font-semibold">
-            ⚠ Si tienes problemas con tu transacción, dirígete al apartado de &quot;Ayuda&quot; o escríbenos a soporte.
+            ⚠ En este momento la red de Worldcoin (Worldchain) presenta una interrupción en sus servicios. Aún no se ha informado una hora estimada para la normalización.
+            <br></br>
+            Las transacciones con ID superior a 43,646 permanecen en estado pendiente.
+            <br></br>
+            Puedes verificar el estado de tu transacción directamente en el historial de Worldcoin, donde aparecerá como “procesando” hasta que la red restablezca su funcionamiento.
+            <br></br>
+            Agradecemos tu comprensión mientras se soluciona la situación.
           </p>
           <p className="font-semibold">
             * Las transacciones que se realicen después de las 10:00 PM se verán reflejadas a partir de las 9:00 AM del día siguiente.
